c9706afdaaa8e37a01601914e403cfc6eeb3ef52
DuDu Silva <raimundoeduardo.silva@gmail.com>
2022-01-30 06:33:02 -0300

    criando arquivos do projeto base

---
+++ pages/404.js
@@ -0,0 +1,4 @@
+export default function Pagina404() {
+    return(<div>404</div>);
+    
+}
---
+++ pages/chat.js
@@ -0,0 +1,218 @@
+import { Box, Text, TextField, Image, Button } from "@skynexui/components";
+import React from "react";
+import appConfig from "../config.json";
+import {createClient} from "@supabase/supabase-js"
+
+export default function ChatPage() {
+  // minha lógica vai aqui
+  const [mensagem, setMensagem] = React.useState("");
+  const [listaMensagem, setListaMensagem] = React.useState([]);
+  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDg2OTA3MywiZXhwIjoxOTU2NDQ1MDczfQ.343ibq7UYFPDdyfsfGmEqUma01RW7P7KC9U2MDAGSkI';
+  const SUPABASE_URL = 'https://kysxypdmtxjlkdysdlas.supabase.co';
+  const supabaseClient = createClient(SUPABASE_URL, SUPABASE_URL);
+  const dadosDoSupabase = supabaseClient
+    .from('mensagens')
+    .select('*')
+    .then((dados)=>{
+      console.log(dados);
+    })
+    
+
+  //Usuario
+  //usuario digita no texarea
+  //aperta enter para enviar
+  //tem que adicionar o texto na lista
+
+  // ./Sua lógica vai aqui
+  //[] Campo criado
+  //[] Usar o onChange
+  //[] Usar o onKey
+
+  function handleNovaMensagem(novaMensagem) {
+    const mensagem = {
+      id: listaMensagem.length,
+      de: "alura",
+      texto: novaMensagem,
+    };
+    setListaMensagem([mensagem,...listaMensagem]);
+    setMensagem("");
+  }
+  return (
+    <Box
+      styleSheet={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(https://images.pexels.com/photos/3745234/pexels-photo-3745234.jpeg)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundBlendMode: "multiply",
+        color: appConfig.theme.colors.neutrals["000"],
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+          borderRadius: "5px",
+          backgroundColor: appConfig.theme.colors.neutrals['500'],
+          height: "100%",
+          maxWidth: "95%",
+          maxHeight: "95vh",
+          padding: "32px",
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{
+            position: "relative",
+            display: "flex",
+            flex: 1,
+            height: "80%",
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: "column",
+            borderRadius: "5px",
+            padding: "16px",
+          }}
+        >
+          {/* {listaMensagem.map((mensagemAtual) =>{
+                        
+                        return(
+                            <li key={mensagemAtual.id}>{mensagemAtual.de}: {mensagemAtual.texto}</li>
+                            
+                            );
+                    })} */}
+          <MessageList mensagens={listaMensagem} />
+
+          <Box
+            as="form"
+            styleSheet={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              value={mensagem}
+              onChange={(evento) => {
+                const valor = evento.target.value;
+                console.log(setMensagem(evento.target.value));
+                setMensagem(valor);
+              }}
+              onKeyPress={(evento) => {
+                if (evento.key === "Enter") {
+                  console.log(evento);
+                  evento.preventDefault();
+                  handleNovaMensagem(mensagem);
+                }
+              }}
+              placeholder="Insira sua mensagem aqui..."
+              type="textarea"
+              styleSheet={{
+                width: "100%",
+                border: "0",
+                resize: "none",
+                borderRadius: "5px",
+                padding: "6px 8px",
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: "12px",
+                color: appConfig.theme.colors.neutrals[900],
+                textColor: appConfig.theme.colors.neutrals[900],
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <Box
+        styleSheet={{
+          width: "100%",
+          marginBottom: "16px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text variant="heading5">Chat</Text>
+        <Button
+          variant="tertiary"
+          colorVariant="neutral"
+          label="Logout"
+          href="/"
+        />
+      </Box>
+    </>
+  );
+}
+
+function MessageList(props) {
+  console.log("MessageList", props);
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: "auto",
+        display: "flex",
+        flexDirection: "column-reverse",
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["000"],
+        marginBottom: "16px",
+      }}
+    >
+      {props.mensagens.map((mensagem) => {
+        return (
+          <Text
+            key={mensagem.id}
+            tag="li"
+            styleSheet={{
+              borderRadius: "5px",
+              padding: "6px",
+              marginBottom: "12px",
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+                
+              },
+            }}
+          >
+            <Box
+              styleSheet={{
+                marginBottom: "8px",
+              }}
+            >
+              <Image
+                styleSheet={{
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  display: "inline-block",
+                  marginRight: "8px",
+                }}
+                src={`https://github.com/alura.png`}
+              />
+              <Text tag="strong">{mensagem.de}</Text>
+              <Text
+                styleSheet={{
+                  fontSize: "10px",
+                  marginLeft: "8px",
+                  color: appConfig.theme.colors.neutrals[300],
+                }}
+                tag="span"
+              >
+                {new Date().toLocaleDateString()}
+              </Text>
+            </Box>
+            {mensagem.texto}
+          </Text>
+        );
+      })}
+    </Box>
+  );
+}
